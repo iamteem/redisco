a8435bb7ef38a3fdb566e09ee97838e7a02ea674
Tim Medina <iamteem@gmail.com>
2010-05-24 15:41:03 +0800

    Adds all_tests.

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,3 +4,10 @@ connect(host='localhost', port=6380, db=10)
 from containers import SetTestCase, ListTestCase, SortedSetTestCase
 from models import ModelTestCase
 
+def all_tests():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(SetTestCase))
+    suite.addTest(unittest.makeSuite(ListTestCase))
+    suite.addTest(unittest.makeSuite(SortedSetTestCase))
+    suite.addTest(unittest.makeSuite(ModelTestCase))
+    return suite
